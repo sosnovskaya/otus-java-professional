85265e5b3a8f21fc5041045d3f219fabed3ac54e
Margarita Sosnovskaya <m.sosnovskaya@qiwi.com>
2024-01-17 22:59:41 +0300

    [hw21-jpql] with one update

--- hw21-jpql/src/main/java/ru/otus/crm/model/Phone.java
+++ hw21-jpql/src/main/java/ru/otus/crm/model/Phone.java
@@ -1,7 +1,48 @@
 package ru.otus.crm.model;
 
-public class Phone {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "phone")
+public class Phone implements Cloneable {
+
+    @Id
+    @SequenceGenerator(name = "phone_gen", sequenceName = "phone_seq", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "phone_gen")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "number")
+    private String number;
+
     public Phone(Long id, String number) {
-        throw new UnsupportedOperationException();
+        this.id = id;
+        this.number = number;
+    }
+
+    @Override
+    @SuppressWarnings({"java:S2975", "java:S1182"})
+    public Phone clone() {
+        return new Phone(this.id, this.number);
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "id=" + id +
+                ", number='" + number + '\'' +
+                '}';
     }
 }

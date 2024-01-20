6ad2cdde8734055bc970de8b921407aac2b8775a
Margarita Sosnovskaya <m.sosnovskaya@qiwi.com>
2024-01-20 13:51:00 +0300

    [hw23-cache] added MyCache implementation

--- hw23-cache/src/main/java/ru/otus/cachehw/MyCache.java
+++ hw23-cache/src/main/java/ru/otus/cachehw/MyCache.java
@@ -1,28 +1,44 @@
 package ru.otus.cachehw;
 
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.WeakHashMap;
+
 public class MyCache<K, V> implements HwCache<K, V> {
+    private final Map<K, V> cache = new WeakHashMap<>();
+    private final List<WeakReference<HwListener<K, V>>> listeners = new ArrayList<>();
+    private final ReferenceQueue<HwListener<K, V>> refQueue = new ReferenceQueue<>();
+
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        cache.putIfAbsent(key, value);
     }
 
     @Override
     public void remove(K key) {
-        throw new UnsupportedOperationException();
+        cache.remove(key);
     }
 
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return cache.get(key);
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        listeners.add(new WeakReference<>(listener, refQueue));
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
-        throw new UnsupportedOperationException();
+        for (int i = 0; i < listeners.size(); i++) {
+            var currentListener = listeners.get(i);
+            if (listener.equals(currentListener)) {
+                listeners.remove(listeners.get(i));
+            }
+        }
     }
 }

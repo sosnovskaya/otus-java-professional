4420aeccf8cd7e7ae98a2e9b838a202c89d41fd7
Margarita Sosnovskaya <m.sosnovskaya@qiwi.com>
2024-01-20 21:56:00 +0300

    [hw23-cache] added MyCache implementation

--- hw23-cache/src/main/java/ru/otus/cachehw/MyCache.java
+++ hw23-cache/src/main/java/ru/otus/cachehw/MyCache.java
@@ -1,6 +1,5 @@
 package ru.otus.cachehw;
 
-import java.lang.ref.ReferenceQueue;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,34 +9,48 @@ import java.util.WeakHashMap;
 public class MyCache<K, V> implements HwCache<K, V> {
     private final Map<K, V> cache = new WeakHashMap<>();
     private final List<WeakReference<HwListener<K, V>>> listeners = new ArrayList<>();
-    private final ReferenceQueue<HwListener<K, V>> refQueue = new ReferenceQueue<>();
 
     @Override
     public void put(K key, V value) {
-        cache.putIfAbsent(key, value);
+        cache.put(key, value);
+        notifyListeners(key, value, "put");
     }
 
     @Override
     public void remove(K key) {
-        cache.remove(key);
+        V value = cache.remove(key);
+        notifyListeners(key, value, "remove");
     }
 
     @Override
     public V get(K key) {
-        return cache.get(key);
+        V value = cache.get(key);
+        notifyListeners(key, value, "get");
+        return value;
     }
 
     @Override
     public void addListener(HwListener<K, V> listener) {
-        listeners.add(new WeakReference<>(listener, refQueue));
+        listeners.add(new WeakReference<>(listener));
     }
 
     @Override
     public void removeListener(HwListener<K, V> listener) {
         for (int i = 0; i < listeners.size(); i++) {
-            var currentListener = listeners.get(i);
+            var currentListener = listeners.get(i).get();
             if (listener.equals(currentListener)) {
                 listeners.remove(listeners.get(i));
+            }
+        }
+    }
+
+    private void notifyListeners(K key, V value, String action) {
+        for (var weakReference : listeners) {
+            var listener = weakReference.get();
+            if (listener != null) {
+                listener.notify(key, null, action);
+            } else {
+                listeners.remove(weakReference);
             }
         }
     }
